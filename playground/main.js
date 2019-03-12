d4c6cf6a97f17b5a3b6afebdff5d72cfb7953f1b
Your Name <michael.erdmann@snafu.de>
2019-03-12 15:37:04 +0100

    refacottored using cell fucions  (tip from mike)

--- playground/main.js
+++ playground/main.js
@@ -5,85 +5,34 @@ document.addEventListener('DOMContentLoaded',function(){
 
             //------------------------------------------------------------------
 		    function toTable( rows ) {
-				
-			    var root = document.getElementById("root");
-			    var tab = document.createElement("table");
 		
-				var tr = document.createElement("tr");		// row 0 where the header goes
-				var th = document.createElement("th");
-				var text = document.createTextNode("Last Name");
-				th.appendChild( text );
-				tr.appendChild(th);
-
-				var th = document.createElement("th");
-				var text = document.createTextNode("First Name");
-				th.appendChild( text );
-				tr.appendChild(th);
-
-				var th = document.createElement("th");
-				var text = document.createTextNode("Party");
-				th.appendChild( text );
-				tr.appendChild(th);
-
-				var th = document.createElement("th");
-				var text = document.createTextNode("Seniority");
-				th.appendChild( text );
-				tr.appendChild(th);
-
-				var th = document.createElement("th");
-				var text = document.createTextNode("Homepage");
-				th.appendChild(text);
-				tr.appendChild(th);
-				
-				tab.appendChild(tr)
+				var tab = document.getElementById("root");
+				tab.setAttribute("class", "table table-striped");
 				
 			    for(var i=0; i<rows.length; ++i) {
 				    var tr = document.createElement("tr");
-
-				    // building a single table row
-
-				    // create  a cell for the first_namd
-                    var td = document.createElement("td");
-                    var text = document.createTextNode(rows[i].first_name);
-				    td.appendChild( text );
-				    tr.appendChild(td);
-
-				    // create a cell for the last name
-				    var td = document.createElement("td");
-				    var text = document.createTextNode(rows[i].last_name);
-				    td.appendChild( text );
-				    tr.appendChild(td);
-
-				    // create a cell with the party
-				    td = document.createElement("td");
-				    text = document.createTextNode(rows[i].party);
-				    td.appendChild(text);
-				    tr.appendChild(td);
-
-				    // create a cell with the seniorty 
-				    td = document.createElement("td");
-				    text = document.createTextNode(rows[i].seniority);
-				    td.appendChild( text );
-				    tr.appendChild(td);
-				
-				    // create a cell for the missed votes.
-				    td = document.createElement("td");
-				    text = document.createTextNode(rows[i].missed_votes);
-				    td.appendChild( text );
-					tr.appendChild(td);
+				    var row  = tab.insertRow(-1);
+					  
+					var  td = row.insertCell(0);
+ 					td.appendChild(document.createTextNode(rows[i].first_name));  				
+					var  td = row.insertCell(-1);
+					td.appendChild(document.createTextNode(rows[i].last_name));
 					
-					// create a cell for the missed votes.
-					td = document.createElement("td");
+					var  td = row.insertCell(-1);
+					td.appendChild(document.createTextNode(rows[i].party));
+
+					var  td = row.insertCell(-1);
+					td.appendChild(document.createTextNode(rows[i].seniority));
+
+
+
+					var  td = row.insertCell(-1);
 					var a = document.createElement("a");
 					a.href = rows[i].url;
 					a.title="some link to " +rows[i].last_name + " page";
 					a.innerHTML = rows[i].url;
-					//a.appendChild(text);
 					td.appendChild(a);
-					//td.appendChild(text);
-					tr.appendChild(td);
 
-            	    // end of cells, append the row to the table.
 				    tab.appendChild(tr);
                 } /* end for */ 
                 root.appendChild(tab);    
