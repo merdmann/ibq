74f71c20b72a2095e44d0983b7ad03071c39ae8a
Your Name <michael.erdmann@snafu.de>
2019-03-27 17:56:11 +0100

    last site sync

--- vue/main.js
+++ vue/main.js
@@ -9,7 +9,8 @@ new Vue({
     data: {
         product: [],
         results: [],
-        states : []
+        state :  [],
+        chwckedParty : ""
     },
     methods: {
         fetchData(){
@@ -27,13 +28,25 @@ new Vue({
                 }
                 ).then( (myJson) => {
                     this.results = myJson.results[0].members;
-                    console.log(this.results)
+                    this.results.forEach( function(items) {
+                        if( this.state.includes(this.results.state)) { 
+                            this.state.push(this.results.state);
+                            console.log( this.state); 
+                        }
+                    })    
                 }).catch(function(err) { console.log(err)})
         },
         properName( data ) {
             // calculate a valid name string
             var name = data.first_name + ' ' + (data.middle_name === null ? ' ' :data.middle_name) + ' ' + data.last_name;
             return name;
+        },
+
+        saveState( aState) {
+            if( !this.state.includes(aState)) {
+                this.state.push(aState);
+            }
+            return aState;
         }
     },
     mounted: function () {
@@ -41,7 +54,14 @@ new Vue({
         this.fetchData()
       // Code that will run only after the
       // entire view has been rendered
-    }
+    },
+    filters: {
+        capitalize: function (value) {
+          if (!value) return ''
+          value = value.toString()
+          return value.charAt(0).toUpperCase() + value.slice(1)
+        }
+      }
 })
 
 function fetchData(congress) {
