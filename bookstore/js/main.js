91fad03bfbcb7c0206a31827c72164b9596439ec
DESKTOP-6ST7TKK\michaelerdmann <michael.erdmann@snafu.de>
2019-04-02 06:50:51 +0200

    Initial Trail

--- bookstore/js/main.js
+++ bookstore/js/main.js
@@ -10,6 +10,44 @@ document.addEventListener('DOMContentLoaded', function () {
 
   main();
 
+    
+          /*<div class="card" style="width: 18rem;">
+                <img id="card_image" class="card-img-top" src="..." alt="Card image cap">
+                <div class="card-body">
+                  <h5 id="titel" class="card-title">Card title</h5>
+                  <p id="card-text" class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+              </div>
+              */
+function placeCard( name, title ) {
+        const root = document.getElementById(name);   
+        console.log(root);
+    
+        var div = document.createElement("div");
+        div.setAttribute("class", "card");
+        div.setAttribute("style", "width: 18rem");
+
+        var card_body = document.createElement("div")
+        card_body.setAttribute("class", "card-body");
+        var h5 =document.createElement("h5")
+        h5.setAttribute("id", "title");
+        card_body.appendChild( h5 )
+        h5.innerHTML = titel;
+        var p = document.createElement("p")
+        p.setAttribute("id", "card-text")
+        p.setAttribute("class","card-title")
+        card_body.appendChild(p);
+        var a = document.creatElement("a")
+        a.setAttribute("class", "btn-primry")
+        a.setAttribute("href", "#"); 
+        card_body.appendChild(a)    
+        root.appendChild(div);
+    
+        return root;
+}
+    
+    
   function ProcessAndRender( data ){
     const books = data["books"]
 
@@ -19,14 +57,18 @@ document.addEventListener('DOMContentLoaded', function () {
         const title = elem.titolo;
     
         console.log(data);
+        
+        var img = document.getElementById("card_image");
+        img.setAttribute("src", frontCover );
+        var card_text = document.getElementById("card-text");
+        card_text.innerHTML = title;
+            
 
-        let cardImage = document.getElementById("card_image");
-        cardImage.setAttribute("src", frontCover); 
-        cardImage.addEventListener("mouseover", function() {
-          console.log("... flip ...");
-        });
-        console.log( data )
-    }) 
+        card.addEventListener("mouseover", function() {
+            console.log("... flip ...");
+        })
+         console.log( data );
+    });
   }
 
   // Needs to be refctored since it hides intention bhind a boolean,
